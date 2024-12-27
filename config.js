9c03c6accb18baa63df09e3ef87f1903cec0c0ca
Hiddennigi <rf038517@gmail.com>
2024-12-27 23:15:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     prefix: process.env.PREFIX || '.', // Command prefix
     ownerName: process.env.OWNER_NAME || 'Toxxic', // Owner name
     ownerNumber: process.env.OWNER_NUMBER || '2348165846414', // Your WhatsApp number
-    mode: process.env.MODE || 'public', // Bot mode: 'public' or 'private'
+    mode: process.env.MODE || 'private', // Bot mode: 'public' or 'private'
     region: process.env.REGION || 'Nigeria', // Region
     botName: process.env.BOT_NAME || 'Rias Gremory V3', // Bot name
     exifPack: process.env.EXIF_PACK || 'RIAS V3 LOVES', // Sticker pack name
